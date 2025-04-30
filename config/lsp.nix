a386afe4f1e1215079ff18a3047cc22556c5b25a
slayer <hi@n.com>
2025-04-30 02:17:11 -0400

    update flake and added more lsp sources

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,15 +1,24 @@
 {
   plugins.luasnip.enable = true;
+  plugins.lspconfig.enable = true;
   plugins.lsp = {
     enable = true;
     servers = {
       ts_ls.enable = true;
+      bashls.enable = true;
+      gh_actions_ls.enable = true;
+      eslint.enable = true;
+      svelte.enable = true;
+      dotls.enable = true;
       lua_ls.enable = true;
       #      rust_analyzer.enable = true;
       docker_compose_language_service.enable = true;
+      terraform_lsp.enable = true;
+      ansiblels.enable = true;
       dockerls.enable = true;
       #graphql.enable = true;
       html.enable = true;
+      sqls.enable = true;
       hydra_lsp = {
         enable = true;
         package = null;
