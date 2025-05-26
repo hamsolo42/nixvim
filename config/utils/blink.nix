35bd669ab9540dc848ce2a581dc73b141f45fddd
slayer <hi@n.com>
2025-05-26 18:43:02 -0400

    added blink avante for file picker

--- config/utils/blink.nix
+++ config/utils/blink.nix
@@ -1,5 +1,12 @@
 # # Source: https://github.com/hmajid2301/dotfiles/blob/ab7098387426f73c461950c7c0a4f8fb4c843a2c/home-manager/editors/nvim/plugins/coding/cmp.nix
 {
+  pkgs,
+  ...
+}:
+{
+  extraPlugins = with pkgs.vimPlugins; [
+    blink-cmp-avante
+  ];
   plugins = {
     blink-cmp.enable = true;
     snacks = {
